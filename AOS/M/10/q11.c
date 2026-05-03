711df3bed0c9aa3f2495373d818fc70900a4083a
Mitesh Gorad <122144445+Mitesh2004@users.noreply.github.com>
2026-05-04 01:36:31 +0530

    Delete AOS directory

--- AOS/M/10/q11.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(){
-    freopen("out.txt","w",stdout);
-    printf("22+12345= %d",(22+12345));
-    printf("This text will be written to the file!\nHello, file redirection works!\n");
-}
-
--- AOS/M/10/q5.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(int c, char *v[]){
-    FILE *f = fopen(v[1],"r");
-    if(f) printf("File exists\n"), fclose(f);
-    else  printf("File not found\n");
-}
-
